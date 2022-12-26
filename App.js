d80edc7343d289405808530760aab49747e48e93
Hussein Omar <husseinomar6190@gmail.com>
2022-12-26 19:28:22 +0300

    refactoring card to its comp

--- App.js
+++ App.js
@@ -2,25 +2,13 @@ import React from 'react';
 import {Image, ImageBackground, StyleSheet, Text, View} from 'react-native';
 
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import Card from './src/components/Card';
 
 const App = () => {
   return (
     // <GestureHandlerRootView>
     <View style={styles.container}>
-      <View style={styles.card}>
-        <ImageBackground
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
-          }}
-          style={styles.image}>
-          <View style={styles.cardInner}>
-            <Text style={styles.name}>Elon Mask</Text>
-            <Text style={styles.bio}>
-              A dude with a rocket is looking for a gal with fuel
-            </Text>
-          </View>
-        </ImageBackground>
-      </View>
+      <Card />
     </View>
     // </GestureHandlerRootView>
   );
@@ -31,43 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  card: {
-    width: '95%',
-    height: '70%',
-    borderRadius: 10,
-    backgroundColor: '#fefefe',
-
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-
-    elevation: 11,
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-
-    justifyContent: 'flex-end',
-  },
-  cardInner: {
-    padding: 10,
-  },
-  name: {
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  bio: {
-    fontSize: 18,
-    color: 'white',
-    lineHeight: 25,
   },
 });
 
